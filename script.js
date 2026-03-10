fa0420fa97c579661b33af3ada9bb8ecf343b20c
Vikash pandit <vikash.pandit0444@gmail.com>
2026-03-10 17:21:49 +0530

    User-Side

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ const noteContent = document.getElementById("note-content");
 
 const saveBtn = document.getElementById("save-btn");
 
-const notesList = document.getElementById("note-list");
+const notesList = document.getElementById("notes-list");
 
 // Local storage
-let userNotes = JSON.parse(localStorage.getItem("notes")) || [];
+// let userNotes = JSON.parse(localStorage.getItem("notes")) || [];
 
 
 // Local storage
 function saveToLocalStorage() {
-    localStorage.setItem(("title", "content"), JSON.stringify(inputNote,contentInput));
+    // Probelm -2 solution
+    localStorage.setItem(("title", "content"), JSON.stringify(inputNote.value,contentInput.value));
 }
 
 function loadFromLocalStorage() {
@@ -36,14 +37,15 @@ function addNote () {
 
     // Create a note object
     const noteObject = {
-  title : noteTitle,
-  content:  noteContent
+        // problem 1 solution
+  title : noteTitle.value,
+  content:  noteContent.value
 };
 
 // Add it to notes arrays 
-if ((title && content) !== "") {
+if (!noteObject) return; {
         // Use the push() method to add the user's input to the array
-        userNotes.push(title && content);
+        userNotes.push(noteObject);
 }
 // Local storage
 
@@ -79,3 +81,64 @@ contentInput();
 // Question:
 
 // 👉 Why should we re-render notes after adding one?
+
+
+let userNotes = [];
+
+function addNote() {
+    //  Get title
+    const title = noteTitle.value.trim();
+    // Get content
+    const content = noteContent.value.trim();
+    // Validate inputs
+     if(!inputNote) return;
+    if(!contentInput) return;
+
+
+    // Create Objects
+   const noteObject =   {
+        title: title,
+        content : content
+    }
+    // add note object to userNotes 
+    userNotes.push(noteObject)
+
+
+    // Save Notes to localStorage
+   localStorage.setItem("notes", JSON.stringify(userNotes));
+}
+
+
+function renderNotes() {
+    // Clean UI
+    notesList.innerHTML = "";
+    userNotes.forEach((note,index) => {
+        const list = document.createElement("li");
+        list.textContent = note.title + " " + note.content
+        // Delete Button
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = index;
+        notesList.appendChild(list);
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// How to Improve Logical Thinking for Coding
+
+// Whenever you get stuck, ask these 4 questions:
+
+// 1️⃣ Where is the data stored?
+// 2️⃣ What new data do I have?
+// 3️⃣ What operation do I want?
+// 4️⃣ Which method does that operation?
